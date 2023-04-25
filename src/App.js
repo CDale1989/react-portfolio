1602ef46123e2ef1dae58118b9cafa57d31a7c3e
Cory <https://github.com/CDale1989/misc.git>
2023-04-25 11:52:43 -0400

    Refined styling / NavBar / Contact form submission(netlify)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
 
 function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 font-bold underline">
+    <main className="text-gray-400 bg-gray-900 font-bold">
       <Navbar />
       <About />
-      {/* <Projects /> */}
+      <Projects />
       {/* <Testimonials /> */}
       <Skills />
       <Contact />
--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function About() {
   return (
-    <section id="about page">
+    <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-xol md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white font-sans">
-            About me
-            <br className="hidden lg:inline-block" />
-            Learn about my journey below.
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white no-underline">
+            Hi, I'm Cory.
+            <br className="hidden lg:inline-block" />I love to build amazing
+            apps.
           </h1>
           <p className="mb-8 leading-relaxed">
             Hello, my name is Cory Bowman. I am 33 years old and I have a
@@ -17,25 +17,25 @@ function About() {
           </p>
           <div className="flex justify-center">
             <a
-              href="contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
             >
-              Reach out to me with questions about my portfolio.
+              Contact Me
             </a>
             <a
-              href="projects"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
             >
-              Check out some of my projects.
+              See My Past Work
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img 
-            className="object-cover object-center rounded"
-            alt="my pic"
-            src={process.env.PUBLIC_URL + '/images/my_pic.jpg'} 
-            />
+        <div className="lg:max-w-md lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-scale-down"
+            alt="hero"
+            src="./images/my_pic.jpg"
+          />
         </div>
       </div>
     </section>
